8d1274d9b1243ce3731b3ec5d36bf9d0d9bed970
MahendranDM <mahendrandm2003@gmail.com>
2026-07-10 21:11:09 +0530

    Added Search

---
+++ src/components/SearchHouseBoat.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const SearchHouseBoat = () => {
+  return (
+    <div>
+       <NavBar/> 
+
+<div className="container">
+                <div className="row">
+                    <div className="col col-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6">
+                                <label htmlFor="" className="form-label">Houseboat ID</label>
+
+                                <input type="text" className="form-control mt-3" />
+
+                                <button className="btn btn-warning mt-3">Search</button>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6"></div>
+                        </div>
+
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default SearchHouseBoat
