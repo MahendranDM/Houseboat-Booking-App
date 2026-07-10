7d14d7c36405e66b20f9b4daf3a78a67e33c92ba
MahendranDM <mahendrandm2003@gmail.com>
2026-07-10 21:10:28 +0530

    Added AddHouseBoat Feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddHouseBoat from './components/AddHouseBoat'
+import DeleteHouseBoat from './components/DeleteHouseBoat'
+import SearchHouseBoat from './components/SearchHouseBoat'
+import ViewHouseBoat from './components/ViewHouseBoat'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +15,23 @@ function App() {
   return (
     <>
      
+<BrowserRouter>
+
+<Routes>
+
+
+<Route path='/' element={<ViewHouseBoat/>}/>
+<Route path='/delete' element={<DeleteHouseBoat/>}/>
+<Route path='/add' element={<AddHouseBoat/>}/>
+<Route path='/search' element={<SearchHouseBoat/>}/>
+
+
+</Routes>
+
+</BrowserRouter>
 
 
 
-     
     </>
   )
 }
--- src/components/AddHouseBoat.jsx
+++ src/components/AddHouseBoat.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const AddHouseBoat = () => {
   return (
     <div>
 
-
+<NavBar/>
 <div className="container mt-4">
   <div className="row">
     <div className="col-12">
